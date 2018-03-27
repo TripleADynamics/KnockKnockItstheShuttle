d327caa0c29d84a8c6b6e90c6f2b9d51975fc6d0
alrom <mfrider2000@gmail.com>
2018-03-27 13:00:30 -0400

    New Neil Armstrong message added.

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.andre.knockknockitstheshuttle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "One small step for a man, one giant leap for mankind.");
     }
 }
