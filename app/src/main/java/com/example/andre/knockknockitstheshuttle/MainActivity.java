048a7630ab100881e33df78d4169b33568530bf9
alrom <mfrider2000@gmail.com>
2018-04-03 11:30:13 -0400

    OnClick method added to the button. Blood, sweat, and tears went into this update.

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.andre.knockknockitstheshuttle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d("Main", "Hey guys its a me a Mario");
         Log.d("Main", "Yeet");
 
+    }
+    public void goToMap(View v)
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        startActivity(intent);
     }
 }
