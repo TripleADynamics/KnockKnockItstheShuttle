bf1ce6556844cf74943431f20ad01038660f182d
awyuan <andrewwyuan@gmail.com>
2018-04-10 08:43:02 -0400

    Update. Rewrote button onClick Methods so that when any button is clicked, the Map Camera moves to that location.

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/MainActivity.java
@@ -5,21 +5,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // Multiple of the same goToMap method for all the different buttons. Methods correspond
+    // in the same order as the xml structure.So 85 Prescott St is goToMap and Faraday Hall is
+    // goToMap 5. The x variable is a holder to essentially listen for what button was pressed.
+    // This variable is placed into the Intent to be extracted in the Map Activity so that it can
+    // go to the proper location
     public void goToMap(View v)
     {
+        x=1;
         Intent intent = new Intent(this, MapActivity.class);
         Log.d("MainActivity", "Switching to Map Activity now.");
-
-        intent.putExtra("latitude", 50.432032);
-        intent.putExtra("longitude", 43.443211);
+        intent.putExtra("MapMove", x);
+        startActivity(intent);
+    }
+    public void goToMap1(View v)
+    {
+        x=2;
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        intent.putExtra("MapMove", x);
+        startActivity(intent);
+    }
+    public void goToMap2(View v)
+    {
+        x=3;
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        intent.putExtra("MapMove", x);
+        startActivity(intent);
+    }
+    public void goToMap3(View v)
+    {
+        x=4;
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        intent.putExtra("MapMove", x);
+        startActivity(intent);
+    }
+    public void goToMap4(View v)
+    {
+        x=5;
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        intent.putExtra("MapMove", x);
+        startActivity(intent);
+    }
+    public void goToMap5(View v) {
+        x=6;
+        Intent intent = new Intent(this, MapActivity.class);
+        Log.d("MainActivity", "Switching to Map Activity now.");
+        intent.putExtra("MapMove", x);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/andre/knockknockitstheshuttle/MapActivity.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/MapActivity.java
@@ -10,19 +10,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
-//import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMyLocationButtonClickListener,
         OnMyLocationClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    int mapLocation=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,12 +48,52 @@ public class MapActivity extends FragmentActivity implements OnMyLocationButtonC
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        // Bundle takes the x variable from Main Activity and sets it to mapLocation to allow it to
+        // move the map camera to the right location.
+        Bundle bundle = getIntent().getExtras();
+        int mapLocation = bundle.getInt("MapMove");
+        //Log.d("Map Activity", Integer.toString(mapLocation));
         mMap = googleMap;
-        Log.d("Map Entry", "I have entered the onMapReady Method");
-        /* Sample Code, We may want to reference later.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+        //Log.d("Map Entry", "I have entered the onMapReady Method");
+
+        // Adds the latitude and longitude values of all the shuttle stops. Latitude and longitude
+        // values were found at latlong.net
+        LatLng Prescott = new LatLng(42.276106, -71.799932);
+        mMap.addMarker(new MarkerOptions().position(Prescott).title("85 Prescott Street"));
+        LatLng Bartlett = new LatLng(42.273727,-71.808906);
+        mMap.addMarker(new MarkerOptions().position(Bartlett).title("Bartlett Center"));
+        LatLng GatewayPark = new LatLng(42.275485,-71.799031);
+        mMap.addMarker(new MarkerOptions().position(GatewayPark).title("Gateway Park"));
+        LatLng Salisbury = new LatLng(42.279726,-71.807448);
+        mMap.addMarker(new MarkerOptions().position(Salisbury).title("Salisbury Estates"));
+        LatLng MainBuilding = new LatLng(42.274575,-71.806272);
+        mMap.addMarker(new MarkerOptions().position(MainBuilding).title("WPI Main Facility Building"));
+        LatLng FaradayHall = new LatLng(42.275159,-71.801032);
+        mMap.addMarker(new MarkerOptions().position(FaradayHall).title("Faraday Hall"));
+
+        //Log.d("Map Activity", Integer.toString(mapLocation));
+
+        // Move camera to the right location. The newLatLngZoom also zooms the camera. I found that
+        // 17 works well for our purposes but you can change these values.
+        if(mapLocation==1) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Prescott, 17));
+        }
+        else if(mapLocation==2){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bartlett,17));
+        }
+        else if(mapLocation==3){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GatewayPark,17));
+        }
+        else if(mapLocation==4){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Salisbury,17));
+        }
+        else if(mapLocation==5){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MainBuilding,17));
+        }
+        else if(mapLocation==6){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(FaradayHall,17));
+        }
 
         boolean check = checkLocationPermission();
         if(check==false){
