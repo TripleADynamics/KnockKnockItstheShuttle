a4bd6d2d6d1711488b1551eb5db64fffe250f306
alrom <mfrider2000@gmail.com>
2018-04-12 20:32:42 -0400

    Risky distanceMatrix update. Beware of pulling...

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/MapActivity.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/MapActivity.java
@@ -2,11 +2,13 @@ package com.example.andre.knockknockitstheshuttle;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,12 +20,17 @@ import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DistanceMatrix;
 
 public class MapActivity extends FragmentActivity implements OnMyLocationButtonClickListener,
         OnMyLocationClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
     int mapLocation=0;
+    String finalEstimatedTime="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +43,41 @@ public class MapActivity extends FragmentActivity implements OnMyLocationButtonC
         mapFragment.getMapAsync(this);
     }
 
+    // Calculates distance and time between two places
+    public void distanceMatrix(double lat, double lng){
+        Log.d("mainActivity", "Entered distanceMatrix() method");
+        // Defines the API key to use
+        String API_KEY = "AIzaSyA0LTJc57HImR70w67fJDuw0S09kpu9DKU";
+        //Provides context for the matrix
+        GeoApiContext context = new GeoApiContext().setApiKey(API_KEY);
+        // If internet permission is not granted, ask for it
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(getBaseContext(), "This app needs internet permission to work", Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions((this), new String[]{Manifest.permission.INTERNET}, 1);
+        }
+        try{
+            DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(context);
+            //Takes the approved request and turns it into an actual distance matrix using the users current lat and long,
+            // and the recipient's address
+            //Matrix takes user's origin as a lat and long value, while it takes the recipients location as a street address
+            DistanceMatrix distanceMatrix = req.origins(lat + "," + lng).destinations(mMap.getMyLocation().getLatitude() + "," + mMap.getMyLocation().getLongitude()).await();
+            //Takes the duration given by distance matrix and writes it to the global variable finalEstimatedTime
+            finalEstimatedTime = ((Long) distanceMatrix.rows[0].elements[0].duration.inSeconds).toString();
+            Log.d("mainActivity", "distanceMatrix functions as desired. Final time: " + finalEstimatedTime);
+        }
+        catch(Exception e){
+            Log.d("mainActivity", "Catching things if they fail!");
+        }}
 
-    /**
-     * Manipulates the map once available.
-     * This callback is TRIGGERED when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        /**
+         * Manipulates the map once available.
+         * This callback is TRIGGERED when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to install
+         * it inside the SupportMapFragment. This method will only be triggered once the user has
+         * installed Google Play services and returned to the app.
+         */
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
@@ -78,6 +110,8 @@ public class MapActivity extends FragmentActivity implements OnMyLocationButtonC
         // 17 works well for our purposes but you can change these values.
         if(mapLocation==1) {
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Prescott, 17));
+            Log.d("mainActivity", "Calling distance matrix and passing it the lat and long of 85 Prescott St.");
+            distanceMatrix(Prescott.latitude,Prescott.longitude);
         }
         else if(mapLocation==2){
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bartlett,17));
