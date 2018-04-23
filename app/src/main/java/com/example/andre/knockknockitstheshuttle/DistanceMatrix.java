a4dbfc9dbe0d08470148ba23183259e49cb7ecfb
awyuan <andrewwyuan@gmail.com>
2018-04-23 09:02:13 -0400

    Commit Jar Files

---
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/DistanceMatrix.java
@@ -0,0 +1,16 @@
+package com.example.andre.knockknockitstheshuttle;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.maps.DistanceMatrixApi;
+import com.google.maps.DistanceMatrixApiRequest;
+import com.google.maps.GeoApiContext;
+
+public class DistanceMatrix {
+}
