099a204cce50906dae9a41fdfd36aafde81ab307
awyuan <andrewwyuan@gmail.com>
2018-05-10 11:49:22 -0400

    Removed random fab toolbar

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/FirstScreen.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/FirstScreen.java
@@ -2,10 +2,7 @@ package com.example.andre.knockknockitstheshuttle;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,17 +15,6 @@ public class FirstScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
     public void goToChoicesScreen(View v)
     {
