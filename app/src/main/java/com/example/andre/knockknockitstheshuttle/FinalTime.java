4fc1005afe4dbe37b656dfaa08c0972ac8a54b94
awyuan <andrewwyuan@gmail.com>
2018-05-11 09:07:16 -0400

    Bunch of changes

--- app/src/main/java/com/example/andre/knockknockitstheshuttle/FinalTime.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/FinalTime.java
@@ -7,62 +7,120 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class FinalTime extends AppCompatActivity {
+
+    private ListView myListView;
+    private ArrayList<String>ShuttleTimes = new ArrayList<>();
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_time);
 
-
-
                 // Read from the database
-                DatabaseReference myRef =FirebaseDatabase.getInstance().getReference("Shuttle Stops");
-                FirebaseApp.initializeApp(this);
-                final Controller aController= (Controller) getApplicationContext();
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                myRef= database.getReference("Shuttle Stops");
+                //FirebaseApp.initializeApp(this);
+                //final Controller aController= (Controller) getApplicationContext();
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference();
+                arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,ShuttleTimes);
+                myListView = (ListView) findViewById(R.id.ShuttleList);
+                myListView.setAdapter(arrayAdapter);
+                Log.d("FinalTime", "In FinalTime.java file");
 
-                myRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-
-                        for(DataSnapshot timesnapshot: dataSnapshot.getChildren()){
-                            FinalTime t = timesnapshot.getValue(FinalTime.class);
-
-                        }
-                        // This method is called once with the initial value and again
-                        // whenever data at this location is updated.
-
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d("FinalTime", "Entered onDataChangeMethod");
+                showData(dataSnapshot);
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
 
 
 
-                        String value = dataSnapshot.getValue(String.class);
-                        Log.d("Main Activity","Value is: " + value);
-                        //TextView FinalTimeText= findViewById(R.id.FinalTimeText);
-                        //FinalTimeText.setText("FInal times"+ value);
-                    }
 
-
-                    @Override
-                    public void onCancelled(DatabaseError error) {
-                        // Failed to read value
-                        Log.w("Main Activity", "Failed to read value.", error.toException());
-                    }
-
-                });
+                //String value = dataSnapshot.getValue(String.class);
+                //Log.d("FinalTime","Value is: " + value);
+                //TextView FinalTimeText= findViewById(R.id.FinalTimeText);
+                //FinalTimeText.setText("FInal times"+ value);
             }
 
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("FinalTime", "Failed to read value.", error.toException());
+            }
+
+
+        });
+
+                myRef.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        /*Log.d("FinalTime", "Entered OnChildAddedListener");
+                        String EstTime = dataSnapshot.getValue(String.class);
+                        ShuttleTimes.add(EstTime);
+                        arrayAdapter.notifyDataSetChanged();*/
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+
+    private void showData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot timesnapshot: dataSnapshot.getChildren()){
+            TimeChecker t = new TimeChecker();
+            t.setLoc1(timesnapshot.getValue(TimeChecker.class).getLoc1());
+            t.setLoc2(timesnapshot.getValue(TimeChecker.class).getLoc2());
+            t.setLoc3(timesnapshot.getValue(TimeChecker.class).getLoc3());
+            t.setLoc4(timesnapshot.getValue(TimeChecker.class).getLoc4());
+            t.setLoc5(timesnapshot.getValue(TimeChecker.class).getLoc5());
+            t.setLoc6(timesnapshot.getValue(TimeChecker.class).getLoc6());
+            t.setLoc7(timesnapshot.getValue(TimeChecker.class).getLoc7());
+            //String EstTime = dataSnapshot.getValue(String.class);
+            //ShuttleTimes.add(EstTime);
+            arrayAdapter.notifyDataSetChanged();
+            //String time = timesnapshot.getValue(String.class);
+            //Log.d("FinalTime", time);
+        }
     }
+
+}
 
 
--- app/src/main/java/com/example/andre/knockknockitstheshuttle/TimeChecker.java
+++ app/src/main/java/com/example/andre/knockknockitstheshuttle/TimeChecker.java
@@ -17,5 +17,71 @@ import com.google.maps.GeoApiContext;
 import com.google.maps.model.DistanceMatrix;
 
 public class TimeChecker {
+    private String Loc1;
+    private String Loc2;
+    private String Loc3;
+    private String Loc4;
+    private String Loc5;
+    private String Loc6;
+    private String Loc7;
 
+    public TimeChecker(){
+
+    }
+
+    public String getLoc1() {
+        return Loc1;
+    }
+
+    public void setLoc1(String loc1) {
+        Loc1 = loc1;
+    }
+
+    public String getLoc2() {
+        return Loc2;
+    }
+
+    public void setLoc2(String loc2) {
+        Loc2 = loc2;
+    }
+
+    public String getLoc3() {
+        return Loc3;
+    }
+
+    public void setLoc3(String loc3) {
+        Loc3 = loc3;
+    }
+
+    public String getLoc4() {
+        return Loc4;
+    }
+
+    public void setLoc4(String loc4) {
+        Loc4 = loc4;
+    }
+
+    public String getLoc5() {
+        return Loc5;
+    }
+
+    public void setLoc5(String loc5) {
+        Loc5 = loc5;
+    }
+
+    public String getLoc6() {
+        return Loc6;
+    }
+
+    public void setLoc6(String loc6) {
+        Loc6 = loc6;
+    }
+
+    public String getLoc7() {
+        return Loc7;
+    }
+
+    public void setLoc7(String loc7) {
+        Loc7 = loc7;
+    }
 }
